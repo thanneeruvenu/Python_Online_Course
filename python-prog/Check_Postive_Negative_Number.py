8ef9398174f2f7b40bdd9c6bce353a9b540a8ea1
Thanneeru, Venu <Venu.Thanneeru@ellucian.com>
2020-01-20 18:46:10 +0530

    Added lab program

--- python-prog/Check_Postive_Negative_Number.py
+++ python-prog/Check_Postive_Negative_Number.py
@@ -1,1 +1,10 @@
 #Python Program to Check Whether a Number is Positive or Negative
+try :
+    n = int(input("enter the value of n"))
+    output = "Negative"
+    if n > 0 :
+        output = "Positive"
+        
+    print("Given {} is %s".format(n)%output)
+except ValueError :
+    print("Enter Valid Number")
